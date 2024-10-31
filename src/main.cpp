e2101d7ad7c864aee3c56c548e3ff68708b9083e
xbghc <1774733158@qq.com>
2024-10-31 16:07:21 +0800

    将Module名称修改为Main

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
-    engine.loadFromModule("ui", "Main");
+    engine.loadFromModule("Main", "Main");
 
     QIcon icon(":/icons/logo");
     app.setWindowIcon(icon);
--- ui/views/SequenceListView.qml
+++ ui/views/SequenceListView.qml
@@ -1,7 +1,7 @@
 pragma ComponentBehavior: Bound
 import QtQuick
 import QtQuick.Controls
-
+import Main
 ListView {
     id: root
 
